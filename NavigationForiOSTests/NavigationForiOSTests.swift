69f5a96e41eebae26a8c8c313d2afeca89cd5b6c
ferretdayo <ferretdayo@hotmail.co.jp>
2017-08-10 19:27:04 +0900

    true case

--- NavigationForiOSTests/NavigationForiOSTests.swift
+++ NavigationForiOSTests/NavigationForiOSTests.swift
@@ -28,7 +28,7 @@ class NavigationForiOSTests: XCTestCase {
     }
     
     func testExample2() {
-        XCTAssertTrue(false)
+        XCTAssertTrue(true)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
