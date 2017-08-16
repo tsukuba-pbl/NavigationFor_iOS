ffb65f0d627a259035954d26e289c631493fb8c0
Minajun <s1620678@u.tsukuba.ac.jp>
2017-08-16 16:53:48 +0900

    [add]implement CLLocation Manager

--- NavigationForiOS/NavigationViewController.swift
+++ NavigationForiOS/NavigationViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 import CoreLocation
 
-class NavigationViewController: UIViewController {
+class NavigationViewController: UIViewController, CLLocationManagerDelegate{
+    
     
     let fruits = ["リンゴ", "みかん", "ぶどう"]
+    var trackLocationManager : CLLocationManager!
+    var beaconRegion : CLBeaconRegion!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // ロケーションマネージャーを作成する
+        self.trackLocationManager = CLLocationManager();
+        
+        //デリゲートを自身に設定
+        self.trackLocationManager.delegate = self;
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 }
