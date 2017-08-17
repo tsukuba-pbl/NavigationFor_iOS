8100f06a7501dabf3eddc514f9a6ff77b65c9d63
Minajun <s1620678@u.tsukuba.ac.jp>
2017-08-17 19:31:56 +0900

    [add]test pattern

--- NavigationForiOSTests/NavigationViewControllerTests.swift
+++ NavigationForiOSTests/NavigationViewControllerTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class NavigationViewControllerTests: XCTestCase {
     
+    let navigationViewController = NavigationViewController()
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -31,6 +33,34 @@ class NavigationViewControllerTests: XCTestCase {
         XCTAssertTrue(true)
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func TestIsOnNavigationPoint_失敗する場合（UUIDが異なる）(){
+        let rssi:Int! = -75
+        let uuid:UUID! = UUID(uuidString : "12345678-1234-1234-1234-123456789ABD")
+        let threshold:Int! = -80
+        XCTAssertFalse(self.navigationViewController.isOnNavigationPoint(RSSI: rssi, uuid: uuid, threshold: threshold))
+    }
+    
+    func TestIsOnNavigationPoint_失敗する場合（閾値よりもRSSIの値が小さい）(){
+        let rssi:Int! = -75
+        let uuid:UUID! = UUID(uuidString : "12345678-1234-1234-1234-123456789ABD")
+        let threshold:Int! = -70
+        XCTAssertFalse(self.navigationViewController.isOnNavigationPoint(RSSI: rssi, uuid: uuid, threshold: threshold))
+    }
+    
+    func TestIsOnNavigationPoint_成功する場合1(){
+        let rssi:Int! = -75
+        let uuid:UUID! = UUID(uuidString : "12345678-1234-1234-1234-123456789ABC")
+        let threshold:Int! = -80
+        XCTAssertTrue(self.navigationViewController.isOnNavigationPoint(RSSI: rssi, uuid: uuid, threshold: threshold))
+    }
+    
+    func TestIsOnNavigationPoint_成功する場合2(){
+        let rssi:Int! = -70
+        let uuid:UUID! = UUID(uuidString : "12345678-1234-1234-1234-123456789ABC")
+        let threshold:Int! = -75
+        XCTAssertTrue(self.navigationViewController.isOnNavigationPoint(RSSI: rssi, uuid: uuid, threshold: threshold))
     }
     
     func testPerformanceExample() {
