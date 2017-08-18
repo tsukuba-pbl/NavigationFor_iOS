54799b6361cdbc1e186ee7b4e220a051b52080cc
Minajun <s1620678@u.tsukuba.ac.jp>
2017-08-18 22:11:02 +0900

    [add]beaconService.swift

---
+++ NavigationForiOS/BeaconService.swift
@@ -0,0 +1,13 @@
+//
+//  BeaconService.swift
+//  NavigationForiOS
+//
+//  Created by みなじゅん on 2017/08/18.
+//  Copyright © 2017年 UmeSystems. All rights reserved.
+//
+
+import Foundation
+
+class BeaconService{
+    
+}
