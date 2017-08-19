10dcf428afcc6a9a7c8b4c5c60c6e411dddadb65
Minajun <s1620678@u.tsukuba.ac.jp>
2017-08-19 11:04:34 +0900

    [modified]BeaconServiceから情報を取得して、ナビゲーションを表示するように変更

--- NavigationForiOS/NavigationViewController.swift
+++ NavigationForiOS/NavigationViewController.swift
@@ -24,6 +24,8 @@ class NavigationViewController: UIViewController, CLLocationManagerDelegate{
     
     var beaconservice : BeaconService!
     
+    let planUUID = "12345678-1234-1234-1234-123456789ABC"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -38,6 +40,12 @@ class NavigationViewController: UIViewController, CLLocationManagerDelegate{
             self.navigationDic = response
             print(self.navigationDic.count)
         }
+        
+        // 初回
+        updateNavigation()
+        
+        // 1秒ごとにビーコンの情報を取得する
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(NavigationViewController.updateNavigation), userInfo: nil, repeats: true)
     }
     
     
@@ -50,6 +58,50 @@ class NavigationViewController: UIViewController, CLLocationManagerDelegate{
         self.rssi.text     = "none"
         self.distance.text = "none"
         self.navigation.text = "none"
+    }
+    
+    //ナビゲーションの更新
+    func updateNavigation(){
+        //現在の最大RSSIのビーコン情報を取得
+        let retval = beaconservice.getMaxRssiBeacon()
+        if(retval.flag == true){
+            self.uuid.text = retval.uuid
+            self.minor.text = "\(retval.minor)"
+            self.rssi.text = "\(retval.rssi)"
+            
+            //ナビゲーションの更新
+            //RSSI最大のビーコンのRSSIの値が-80dB以下のとき、案内が表示されるようにする
+            if(isOnNavigationPoint(RSSI: retval.rssi, uuid: UUID(uuidString : retval.uuid)!, threshold: -80)){
+                //self.navigation.text = "交差点だから曲がろう"
+                let navigationText = navigationDic[retval.minor]
+                if(navigationText != nil){
+                    self.navigation.text = navigationText
+                }else{
+                    self.navigation.text = "ルート上から外れている可能性があります"
+                }
+            }else{
+                self.navigation.text = "進もう"
+            }
+
+        }else{
+            reset()
+        }
+    }
+
+    //ナビゲーションを行うタイミングを判定する
+    //目的地もしくは交差点にいるかを判定する
+    /// - Parameters:
+    ///   - RSSI: 最大RSSIのビーコンのRSSI
+    ///   - uuid: 最大RSSIのビーコンのuuid
+    ///   - threshold : 閾値（RSSI）
+    /// - Returns: 入力が正しければtrue，正しくなければfalse
+    func isOnNavigationPoint(RSSI : Int, uuid : UUID, threshold : Int) -> Bool {
+        var flag: Bool = false
+        //使用するUUIDと一致しており、かつ閾値よりも大きいRSSI
+        if(uuid.uuidString == planUUID && RSSI > threshold){
+            flag = true
+        }
+        return flag
     }
     
     
