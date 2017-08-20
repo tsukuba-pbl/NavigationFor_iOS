537ce18472cd3b8669a2d31197ebcdab70dd5477
Minajun <s1620678@u.tsukuba.ac.jp>
2017-08-20 09:36:07 +0900

    [add]Test for BeaconService

---
+++ NavigationForiOSTests/BeaconServiceTests.swift
@@ -0,0 +1,39 @@
+//
+//  BeaconServiceTests.swift
+//  NavigationForiOS
+//
+//  Created by みなじゅん on 2017/08/20.
+//  Copyright © 2017年 UmeSystems. All rights reserved.
+//
+
+import XCTest
+@testable import NavigationForiOS
+
+class BeaconServiceTests: XCTestCase {
+    let beaconservice = BeaconService()
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testGetUsingUUID() {
+        let uuid = beaconservice.getUsingUUID()
+        XCTAssertEqual(uuid, "12345678-1234-1234-1234-123456789ABC")
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
