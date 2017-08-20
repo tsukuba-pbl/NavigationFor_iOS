ea24aa4faeeb4a5acf8eef91e15ee1834ac90bce
Minajun <s1620678@u.tsukuba.ac.jp>
2017-08-20 09:36:07 +0900

    [add]Test for BeaconService

--- NavigationForiOSTests/NavigationViewControllerTests.swift
+++ NavigationForiOSTests/NavigationViewControllerTests.swift
@@ -16,6 +16,7 @@ class NavigationViewControllerTests: XCTestCase {
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        navigationViewController.viewDidLoad()
     }
     
     override func tearDown() {
