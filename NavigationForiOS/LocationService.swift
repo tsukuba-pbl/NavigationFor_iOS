c25d15bb34d7179831420ddf0bf6706821c72b75
ferretdayo <ferretdayo@hotmail.co.jp>
2017-08-21 22:27:43 +0900

    change url

--- NavigationForiOS/LocationService.swift
+++ NavigationForiOS/LocationService.swift
@@ -15,7 +15,7 @@ class LocationService {
     ///
     /// - Returns: 地点を含む配列
     static func getLocations(responseLocations: @escaping ([String]) -> Void){
-        Alamofire.request("https://gist.githubusercontent.com/ferretdayo/052e93d7c3067832e39f5ebe8cbfb004/raw/ee81892056eed628852ea6a92144232f5719d0f4/location.json")
+        Alamofire.request("https://gist.githubusercontent.com/ferretdayo/052e93d7c3067832e39f5ebe8cbfb004/raw/885aae273ae6b5d78380b917357e6827dba0de70/location.json")
         .responseJSON { response in
             var locations: [String] = []
             switch response.result {
