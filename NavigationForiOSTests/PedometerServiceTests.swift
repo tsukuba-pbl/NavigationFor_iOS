cf957dd134f8f5577e7549f7afcb684094067e64
Ikuzus-k <ae14suzuki@g.kumamoto-nct.ac.jp>
2017-08-26 16:52:58 +0900

    いらない部分削除

--- NavigationForiOSTests/PedometerServiceTests.swift
+++ NavigationForiOSTests/PedometerServiceTests.swift
@@ -23,18 +23,6 @@ class PedometerServiceTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testIsStepEqual0_初期歩数が0であること() {
         XCTAssertEqual(self.pedometerservice.get_steps(), 0)
     }
