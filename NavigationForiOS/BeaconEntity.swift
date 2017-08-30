b05292fdff83209f6fc265a958549baa8e9eb17f
Minajun <s1620678@u.tsukuba.ac.jp>
2017-08-30 12:43:49 +0900

    [add]BeaconEntity class

--- NavigationForiOS/BeaconEntity.swift
+++ NavigationForiOS/BeaconEntity.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class BeaconEntity{
+    var minorId : Int!
+    var rssi : Int!
+    var uuid : String!
+    
+    init(){
+        self.minorId = -1
+        self.rssi = -100
+        self.uuid = ""
+    }
+    
+    init(minorId: Int, rssi: Int){
+        self.minorId = minorId
+        self.rssi = rssi
+        self.uuid = ""
+    }
+    
+    init(minorId: Int, rssi: Int, uuid: String){
+        self.minorId = minorId
+        self.rssi = rssi
+        self.uuid = uuid
+    }
+}
