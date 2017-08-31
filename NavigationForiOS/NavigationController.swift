560f2032d419e0fa4ed648f24293bdac20d63081
Minajun <s1620678@u.tsukuba.ac.jp>
2017-08-31 14:07:13 +0900

    [add]updateNavigationのテンプレを記述

--- NavigationForiOS/NavigationController.swift
+++ NavigationForiOS/NavigationController.swift
@@ -9,29 +9,47 @@
 import Foundation
 
 class NavigationController{
+    updateNavigation(navigationController: NavigationController, navigationEntity: NavigationEntity) -> (mode : Int, maxRssiBeacon: BeaconEntity, navigation_text : String){
     
+    }
 }
 
 protocol State {
-    
+    func updateNavigation(navigationController: NavigationController, navigationEntity: NavigationEntity) -> (mode : Int, maxRssiBeacon: BeaconEntity, navigation_text : String)
 }
 
 //前進状態
 class GoFoward: State{
+    func updateNavigation(navigationController: NavigationController, navigationEntity: NavigationEntity) -> (mode: Int, maxRssiBeacon: BeaconEntity, navigation_text: String) {
+        <#code#>
+    }
+
     
 }
 
 //交差点到達状態
 class OnThePoint: State{
+    func updateNavigation(navigationController: NavigationController, navigationEntity: NavigationEntity) -> (mode: Int, maxRssiBeacon: BeaconEntity, navigation_text: String) {
+        <#code#>
+    }
+
     
 }
 
 //右左折待機状態
 class WaitTurn: State{
+    func updateNavigation(navigationController: NavigationController, navigationEntity: NavigationEntity) -> (mode: Int, maxRssiBeacon: BeaconEntity, navigation_text: String) {
+        <#code#>
+    }
+
     
 }
 
 //目的地到達状態
 class Goal: State{
+    func updateNavigation(navigationController: NavigationController, navigationEntity: NavigationEntity) -> (mode: Int, maxRssiBeacon: BeaconEntity, navigation_text: String) {
+        <#code#>
+    }
+
     
 }
