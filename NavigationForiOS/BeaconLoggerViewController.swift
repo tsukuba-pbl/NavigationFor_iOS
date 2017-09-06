098eaee968655973d2395527b737a83b0bf6ce5b
Minajun <s1620678@u.tsukuba.ac.jp>
2017-09-06 23:38:55 +0900

    [add]ビーコンの受信機能を追加

--- NavigationForiOS/BeaconLoggerViewController.swift
+++ NavigationForiOS/BeaconLoggerViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class BeaconLoggerViewController: UIViewController {
     @IBOutlet weak var startButton: UIButton! //計測開始ボタン
+    
+    var navigations : NavigationEntity!
+    var beaconManager : BeaconManager = BeaconManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         //最初はスタートボタンは押せる状態
         startButton.isEnabled = true
-        // Do any additional setup after loading the view.
+        
+        //使用するビーコンのminor idを設定する
+        //本当はここで、JSONとかを読み込んで設定したい
+        for i in 1...9{
+            navigations.MinorIdList.append(i)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +37,8 @@ class BeaconLoggerViewController: UIViewController {
         startButton.isEnabled = false
         startButton.setTitle("計測中", for: UIControlState.normal)
         startButton.backgroundColor = UIColor.red
+        //受信するビーコンの情報を与え、受信を開始する
+        beaconManager.startBeaconReceiver(navigations: self.navigations)
     }
 
 }
