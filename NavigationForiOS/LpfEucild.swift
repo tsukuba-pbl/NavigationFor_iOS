582872bd30f7a73fe932bb337f1a26732ba6a28c
Minajun <s1620678@u.tsukuba.ac.jp>
2017-09-06 14:46:10 +0900

    [add]ビーコンまでの距離を計算するメソッドを実装

--- NavigationForiOS/LpfEucild.swift
+++ NavigationForiOS/LpfEucild.swift
@@ -16,4 +16,13 @@ class LpfEuclid: AlgorithmBase {
     func getRouteId(navigations: NavigationEntity, receivedBeaconsRssi: Dictionary<Int, Int>) -> Int {
         return 1;
     }
+    
+    /// 電波強度から、ビーコンまでの距離を計算する関数
+    ///
+    /// - Parameter rssi: ビーコンの電波強度（dB）
+    /// - Returns: ビーコンまでの距離 (m)
+    func getBeaconDist(rssi: Int) -> Double{
+        let TxPower = -75.0 //ビーコンから1mの距離で観測される電波強度
+        return pow(10.0, (TxPower - Double(rssi)) / 20.0)
+    }
 }
--- NavigationForiOSTests/LpfEuclidTests.swift
+++ NavigationForiOSTests/LpfEuclidTests.swift
@@ -6,4 +6,34 @@
 //  Copyright © 2017年 UmeSystems. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import NavigationForiOS
+
+class LpfEuclidTests: XCTestCase {
+    let lpfEuclid = LpfEuclid()
+    
+    override func setUp() {
+        super.setUp()
+
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testGetBeaconDist(){
+        //小数点第一位まで値が同じかをテストする
+        XCTAssertEqual(round(lpfEuclid.getBeaconDist(rssi: -75)*10.0)/10.0, 1.0)
+        XCTAssertEqual(round(lpfEuclid.getBeaconDist(rssi: -80)*10.0)/10.0, 1.8)
+
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
