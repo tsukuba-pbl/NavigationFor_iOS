485b46accb8345e666b5b94f5d0bd5c7c1a1110d
ferretdayo <ferretdayo@hotmail.co.jp>
2017-09-25 13:26:39 +0900

    speechの部分のコメントアウトを外した．恐らくこれで動く？

--- NavigationForiOS/NavigationService.swift
+++ NavigationForiOS/NavigationService.swift
@@ -15,7 +15,7 @@ class NavigationService {
     var initRouteId = 1
     
     //ステートマシンの状態
-    var expectedRouteId = 0
+    var currentRouteId = 1
     
     //初期状態を設定
     var navigationState: NavigationState
@@ -100,10 +100,10 @@ class NavigationService {
         let navigationStateMachineProperty = navigationState.getNavigationState()
         
         //音声案内(ステートマシンの状態が遷移したら)
-//        if(navigationStateMachineProperty.state == "OnThePoint" && navigationStateMachineProperty.expectedRouteId != expectedRouteId){
-//            speechService.textToSpeech(str: navigation_text)
-//            expectedRouteId = navigationStateMachineProperty.expectedRouteId
-//        }
+        if(navigationStateMachineProperty.currentRouteId != self.currentRouteId){
+            speechService.textToSpeech(str: navigation_text)
+            self.currentRouteId = navigationStateMachineProperty.currentRouteId
+        }
         
         return (mode, navigation_text, navigationStateMachineProperty.state, navigationStateMachineProperty.currentRouteId)
     }
