5293d5883693b041c2fc9435d570de51879f213a
Minajun <s1620678@u.tsukuba.ac.jp>
2017-10-18 11:42:46 +0900

    [add]LostDetectService.swift

---
+++ NavigationForiOS/LostDetectService.swift
@@ -0,0 +1,13 @@
+//
+//  LostDetectService.swift
+//  NavigationForiOS
+//
+//  Created by みなじゅん on 2017/10/18.
+//  Copyright © 2017年 UmeSystems. All rights reserved.
+//
+
+import Foundation
+
+class LostDetectService{
+    
+}
