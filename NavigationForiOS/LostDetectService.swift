5b3b7bc5276854a1c25e679e70782a9bd9a9bed2
Minajun <s1620678@u.tsukuba.ac.jp>
2017-10-18 11:53:41 +0900

    [add]迷っていることを検知するメソッドを作成

--- NavigationForiOS/LostDetectService.swift
+++ NavigationForiOS/LostDetectService.swift
@@ -9,5 +9,21 @@
 import Foundation
 
 class LostDetectService{
+    //歩数計測用サービス
+    let pedometerService = PedometerService()
     
+    
+    /// 迷っていることを検知する処理
+    ///
+    /// - Parameters:
+    ///   - navigations: ナビゲーション情報
+    ///   - currentRouteId: 現在のroute id
+    ///   - statemachineState: ステートマシンの状態コード
+    ///   - receivedBeaconRssiList: 受信しているビーコンのRSSIリスト
+    /// - Returns: チェックの結果
+    func checkLost(navigations : NavigationEntity, currentRouteId: Int, statemachineState : Int, receivedBeaconRssiList : Dictionary<Int, Int>) -> Int{
+        var retval = 0
+        
+        return retval
+    }
 }
