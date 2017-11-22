83aa8694d6433bb1bb6693ca299a2259de60e198
shunsukeumemura <umes0714@gmail.com>
2017-11-22 15:10:18 +0900

    設定をhttpに戻した

--- NavigationForiOS/LocationService.swift
+++ NavigationForiOS/LocationService.swift
@@ -10,60 +10,17 @@ import Foundation
 import Alamofire
 import SwiftyJSON
 import UIKit
-//
-//class Manager {
-//    static let sharedManager: Alamofire.SessionManager = {
-//
-//        // Create the server trust policies
-//        let serverTrustPolicies: [String: ServerTrustPolicy] = [
-//            "example.com:80": .disableEvaluation
-//        ]
-//
-//        // Create custom manager
-//        let configuration = URLSessionConfiguration.default
-//        configuration.httpAdditionalHeaders = Alamofire.SessionManager.defaultHTTPHeaders
-//        let manager = Alamofire.SessionManager(
-//            configuration: URLSessionConfiguration.default,
-//            serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
-//        )
-//        return manager
-//    }()
-//    private init() {
-//    }
-//}
-
 
 class LocationService {
-    
     /// 会場にある各地点の名前を取得
     ///
     /// - Returns: 地点を含む配列
     static func getLocations(responseLocations: @escaping ([String]) -> Void){
 
-        let sharedManager: Alamofire.SessionManager = {
-            
-            // Create the server trust policies
-            let serverTrustPolicies: [String: ServerTrustPolicy] = [
-                "localhost:80": .pinCertificates(
-                    certificates: ServerTrustPolicy.certificates(),
-                    validateCertificateChain: false,
-                    validateHost: true
-                ),
-            ]
-            
-            // Create custom manager
-            let configuration = URLSessionConfiguration.default
-            configuration.httpAdditionalHeaders = Alamofire.SessionManager.defaultHTTPHeaders
-            let manager = Alamofire.SessionManager(
-                configuration: URLSessionConfiguration.default,
-                serverTrustPolicyManager: ServerTrustPolicyManager(policies: serverTrustPolicies)
-            )
-            return manager
-        }()
         let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
         let eventInfo = appDelegate.eventInfo!
         if let eventId = eventInfo.id {
-            Alamofire.request("https://localhost/api/events/0kzrV/locations")
+            Alamofire.request("http://localhost/api/events/\(eventId)/locations")
                 .responseJSON { response in
                     debugPrint(response)
                     var locations: [String] = []
