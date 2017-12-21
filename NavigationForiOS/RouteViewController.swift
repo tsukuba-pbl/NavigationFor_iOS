2e2678f55adc72f5a0836c33bc1652d269fe5beb
ferretdayo <ferretdayo@hotmail.co.jp>
2017-12-21 14:31:57 +0900

    fix ui

--- NavigationForiOS/RouteViewController.swift
+++ NavigationForiOS/RouteViewController.swift
@@ -34,8 +34,8 @@ class RouteViewController: FormViewController {
         form
         +++ Section("Source")
             <<< PushRow<String>("SourceLocations"){
-                $0.title = "現在地"
-                $0.selectorTitle = "現在地の選択"
+                $0.title = "出発地"
+                $0.selectorTitle = "出発地の選択"
                 $0.options = self.point
                 $0.onChange{[unowned self] row in
                     self.departure = row.value ?? self.point[0]
